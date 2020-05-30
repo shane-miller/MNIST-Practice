9bf4c8ebfd27d950d6be761379aec095ea0494ed
Shane Miller <shanekmiller44@gmail.com>
2020-05-30 17:14:21 -0400

    changed network again

--- network.py
+++ network.py
@@ -89,23 +89,27 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1),
+            nn.Conv2d(1, 32, 3, stride=1),
             nn.ReLU(),
-            nn.Dropout(p=0.4),
-            nn.BatchNorm2d(16)
+            nn.MaxPool2d((2, 2), stride=2)
         )
 
-        self.fcDropout = nn.Dropout(p=0.5)
-        self.fc1 = nn.Linear(16 * 28 * 28, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), stride=2)
+        )
+
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.conv2(x)
 
         x = x.view(x.size(0), -1)
 
         x = F.relu(self.fc1(x))
-        x = self.fcDropout(x)
         x = F.softmax(self.fc2(x), dim=1)
 
         return x
